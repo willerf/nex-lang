9dd7244f3977237e8e4c6d80312b3375d5414436
willerf <erfwill@gmail.com>
2023-10-17 19:00:38 -0700

    bin op rename

---
+++ src/program_representation/bin_op.cc
@@ -0,0 +1,63 @@
+
+#include "bin_op.h"
+
+#include "variable.h"
+#include "code_structures/var_access.h"
+#include "code_structures/scope.h"
+
+std::shared_ptr<Code> bin_op(std::shared_ptr<Code> e1, std::shared_ptr<Code> op, std::shared_ptr<Code> e2) {
+    std::shared_ptr<Variable> var = std::make_shared<Variable>("bin_op variable");
+    return make_scope({var},
+        make_block({
+            e1,
+            make_write(var, Reg::Result),
+            e2,
+            make_read(Reg::Scratch, var),
+            op
+        })
+    );
+}
+
+std::shared_ptr<Code> op::plus() {
+    return make_add(Reg::Result, Reg::Scratch, Reg::Result);
+}
+
+std::shared_ptr<Code> op::minus() {
+    return make_sub(Reg::Result, Reg::Scratch, Reg::Result);
+}
+
+std::shared_ptr<Code> op::times() {
+    return make_block({
+        make_mult(Reg::Scratch, Reg::Result),
+        make_mflo(Reg::Result)
+    });
+}
+
+std::shared_ptr<Code> op::divide() {
+    return make_block({
+        make_div(Reg::Scratch, Reg::Result),
+        make_mflo(Reg::Result)
+    });
+}
+
+std::shared_ptr<Code> op::remainder() {
+    return make_block({
+        make_div(Reg::Scratch, Reg::Result),
+        make_mfhi(Reg::Result)
+    });
+}
+
+std::shared_ptr<Code> op::divide_unsigned() {
+    return make_block({
+        make_divu(Reg::Scratch, Reg::Result),
+        make_mflo(Reg::Result)
+    });
+}
+
+std::shared_ptr<Code> op::remainder_unsigned() {
+    return make_block({
+        make_divu(Reg::Scratch, Reg::Result),
+        make_mfhi(Reg::Result)
+    });
+}
+
---
+++ src/program_representation/bin_op.h
@@ -0,0 +1,17 @@
+
+#include "code.h"
+#include "code_structures/block.h"
+#include "assembly.h"
+
+std::shared_ptr<Code> bin_op(std::shared_ptr<Code> e1, std::shared_ptr<Code> op, std::shared_ptr<Code> e2);
+
+namespace op {
+    std::shared_ptr<Code> plus();
+    std::shared_ptr<Code> minus();
+    std::shared_ptr<Code> times();
+    std::shared_ptr<Code> divide();
+    std::shared_ptr<Code> remainder();
+    std::shared_ptr<Code> divide_unsigned();
+    std::shared_ptr<Code> remainder_unsigned();
+}
+
